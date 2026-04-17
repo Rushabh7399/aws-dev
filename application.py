5193cb58bd573321a1db842b764e1180efacceff
Rushabh Shah <37475271+Rushabh7399@users.noreply.github.com>
2026-04-18 00:35:21 +0530

    Check connection for beanstalk

--- application.py
+++ application.py
@@ -1,45 +1,7 @@
-from fastapi import FastAPI, Form, HTMLResponse
-import psycopg2
+from fastapi import FastAPI
 
 app = FastAPI()
 
-DB_HOST = "database-1.cj2ccse0elz9.ap-southeast-2.rds.amazonaws.com"
-DB_NAME = "postgres"
-DB_USER = "postgres"
-DB_PASS = "rashah99"
-
-def get_connection():
-    return psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS
-    )
-
-@app.get("/", response_class=HTMLResponse)
-def form():
-    return """
-    <html>
-        <body>
-            <form action="/submit" method="post">
-                Name: <input type="text" name="name"><br>
-                Password: <input type="text" name="password"><br>
-                <input type="submit">
-            </form>
-        </body>
-    </html>
-    """
-
-@app.post("/submit")
-def submit(name: str = Form(...), password: str = Form(...)):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("CREATE TABLE IF NOT EXISTS users (name TEXT, password TEXT);")
-    cur.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (name, password))
-
-    conn.commit()
-    cur.close()
-    conn.close()
-
-    return {"message": "Data inserted successfully"}
+@app.get("/")
+def test():
+    return {"status": "working"}
